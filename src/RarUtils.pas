f19606a03f3e9d4bdf61f3f0ed223c0672d43d01
unknown <renato__ferreira@hotmail.com>
2023-09-19 15:59:22 -0300

    changed extractfile function to wait until finish

--- src/RarUtils.pas
+++ src/RarUtils.pas
@@ -3,16 +3,18 @@ unit RarUtils;
 interface
 
 //procedure CompressFiles(aFile, aDestinationPath: string);
-function ExtractFile(aFile, aDestinationPath: string): Boolean;
+function ExtractFile(aRarFilePath, aDestinationPath: string): Boolean;
 
 implementation
 
 uses
-  Winapi.ShellAPI, System.SysUtils;
+  Winapi.Windows, Winapi.ShellAPI, System.SysUtils;
 
-function ExtractFile(aFile, aDestinationPath: string): Boolean;
+function ExtractFile(aRarFilePath, aDestinationPath: string): Boolean;
 var
-  sWinRarExe, sParam: string;
+  stInfo: TStartupInfo;
+  piProcessInfo: TProcessInformation;
+  sWinRarExe, sCmdLine: string;
 begin
   sWinRarExe := 'C:\Program Files\WinRAR\WinRAR.exe';
 
@@ -22,8 +24,22 @@ begin
   if not FileExists(sWinRarExe) then
     raise Exception.Create('Erro instalação winrar não encontrado');
 
-  sParam := 'x -ibck "' + aFile + '" "' + aDestinationPath + '"';
-  Result := ShellExecute(MainInstance, 'Open', PChar(sWinRarExe), PChar(sParam), '', 0) > 32;
+  sCmdLine := '"' + sWinRarExe + '" x -ibck "' + aRarFilePath + '" "' + aDestinationPath + '"';
+
+  FillChar(stInfo, SizeOf(stInfo), #0);
+
+  stInfo.cb := SizeOf(stInfo);
+  stInfo.dwFlags := STARTF_USESHOWWINDOW;
+  stInfo.wShowWindow := SW_SHOW;
+
+  Result := CreateProcess(nil, PChar(sCmdLine), nil, nil, False, CREATE_NEW_CONSOLE or NORMAL_PRIORITY_CLASS, nil, PChar(ExtractFilePath(ParamStr(0))), stInfo, piProcessInfo);
+
+  if Result then begin
+    WaitForSingleObject(piProcessInfo.hProcess, INFINITE);
+
+    CloseHandle(piProcessInfo.hProcess);
+    CloseHandle(piProcessInfo.hThread);
+  end;
 end;
 
 end.
